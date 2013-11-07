fdaa6baf7a24f1d0d4c720b7c128ec27882e448b
FisHinLab <fishinlab@sina.com>
2013-11-07 10:55:49 +0800

    docker install script updated

--- others/sh.shell/docker/install_docker.sh
+++ others/sh.shell/docker/install_docker.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo sh -c "wget -qO- https://get.docker.io/gpg | apt-key add -"
-sudo sh -c "echo deb http://git.docker.io/ubuntu docker main/"
+sudo sh -c "echo deb http://git.docker.io/ubuntu docker main /etc/apt/sources.list.d/docker.list"
 sudo apt-get update
 sudo apt-get install lxc-docker
 sudo docker run -i -t ubuntu /bin/bash 
